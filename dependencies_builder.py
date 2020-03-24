4ebf210e7779bf4f0791e3f90d750cd8456c0122
Ture2 <alberto_tureganocsb@hotmail.com>
2020-03-24 10:04:34 +0100

    Merge files

--- dependencies_builder.py
+++ dependencies_builder.py
@@ -1,5 +1,4 @@
 import constants
-<<<<<<< HEAD
 import mdcontracts
 import os
 import logging
@@ -32,31 +31,6 @@ def doc_creation(contract):
         f.write(content)
         f.close()
 
-=======
-from pymongo import MongoClient
-
-# HOST
-
-TOOL = ['SOLGRAPH', 'OYENTE']
-
-# TODO: Cambiar el path para leer el actual
-PATH_TO_MAIN_DIRECTORY = '/Users/Ture/Documents/Blockchain/ANT-BERTO'
-PATH_TO_TMP = '/tmp'
-PATH_TO_OUTPUT = '/outputs'
-PATH_TO_VOLUME = '/var/lib/docker/volumes/outputs/_data'
-
-
-def get_contract(id):
-    client = MongoClient("127.0.0.1",
-                         27017)  # OJO! La IP de MongoDB aqui es 127.0.0.1. (Desde dentro de un docker es 172.17.0.1, que es la IP del anfitrion)
-    db = client.my
-    collection = db[constants.MONGO_COLLECTION]
-    cursor = collection.find({'contract_id': id})
-    return cursor.next()
-
-
-def get_content(tool, id):
->>>>>>> parent of 723aaa0... v0.2 Solgraph and Oyente works correctly, refactored code
 
     if tool.upper() in ['SOLGRAPH']:
         file_extension = 'sol'
@@ -68,7 +42,6 @@ def get_content(tool, id):
         content = get_contract(id)['bytecode']
     return content, file_extension
 
-<<<<<<< HEAD
 # TODO: modificar el dirname, ahora mismo usa el path to main, si se usa una ruta custom que no este dentro del path fallara
 def save_results(content, output_file, tool, parent_dir):
     os.chdir(parent_dir)
@@ -99,16 +72,3 @@ def create_output_dir(path):
         logger.info('Succesfully output path created -> {}'.format(path))
     else:
         logger.warning('The following path already exits {}.'.format(path))
-=======
-def doc_creation(tool, id):
-    code, file_extension = get_content(tool, id)
-    if code:
-        generic_file = "tmp_{}_{}".format(tool, id)
-        path_to_input_file = PATH_TO_MAIN_DIRECTORY + PATH_TO_OUTPUT + "/{}.{}".format(generic_file, file_extension)
-        f = open(path_to_input_file, "w+")
-        f.write(code)
-        f.close()
-        return generic_file
-    else:
-        return False
->>>>>>> parent of 723aaa0... v0.2 Solgraph and Oyente works correctly, refactored code
